1f212d854241252369b481b24075a31b1bd41460
Mateusz Paruzel <mati.paruzel@gmail.com>
2024-08-25 12:02:06 +0200

    updated githubService

--- src/main/java/com/example/github_popular_repos/service/IGithubService.java
+++ src/main/java/com/example/github_popular_repos/service/IGithubService.java
@@ -5,5 +5,5 @@ import com.example.github_popular_repos.model.GithubRepository;
 import java.util.List;
 
 public interface IGithubService {
-   public List<GithubRepository> getPopularRepositories(String date, String language, int limit);
+   List<GithubRepository> getPopularRepositories(String date, String language, int limit);
 }
